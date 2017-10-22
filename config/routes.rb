2a29203da1fad9d3a05fe5917b92f1357f902a56
ELVIS PETER <elvispeter91@gmail.com>
2017-10-22 19:13:18 -0400

    --crud- --crud--

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  delete 'pictures/:id' => 'pictures#destory'
   get 'pictures' => 'pictures#index'
+  post 'pictures' => 'pictures#create'
+  get 'pictures/new'=>'pictures#new'
+  get 'pictures/:id' => 'pictures#show'
+  get 'pictures/:id/edit' => "pictures#edit"
+  patch 'pictures/:id' => "pictures#update"
+
+  #resources :pictures
 end
